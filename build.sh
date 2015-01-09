92868fd5af86b29c99ce686bfbd70440df07abce
Matthew Honnibal <honnibal@gmail.com>
2015-01-10 08:41:36 +1100

    * Upd build for different corenlp

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -eux
 rm -rf _build lib/piperunner.jar
 mkdir _build
 
-javac -source 7 -target 7 -d _build -cp "$(print lib/*.jar | tr ' ' ':')":../stanford-corenlp-3.3.1.jar javasrc/**/*.java
+javac -source 8 -target 8 -d _build -cp "$(print lib/*.jar | tr ' ' ':')":../stanford-corenlp-full-2014-10-31/stanford-corenlp-3.5.0.jar javasrc/**/*.java
 (cd _build && jar cf ../lib/piperunner.jar .)
 ls -l lib/piperunner.jar
